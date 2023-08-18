627386e0363ae22b7f8c1bb7880813957ea2efc7
wang <quang07112004@gmail.com>
2023-08-19 00:54:44 +0700

    congif

--- next.config.js
+++ next.config.js
@@ -6,4 +6,16 @@ module.exports = {
   images: {
     domains: ["static.nike.com"],
   },
+  webpack: (config) => {
+    let modularizeImports = null;
+    config.module.rules.some((rule) =>
+      rule.oneOf?.some((oneOf) => {
+        modularizeImports = oneOf?.use?.options?.nextConfig?.modularizeImports;
+        return modularizeImports;
+      })
+    );
+    if (modularizeImports?.["@headlessui/react"])
+      delete modularizeImports["@headlessui/react"];
+    return config;
+  },
 };
